e863bf84c228873fcdd9fedf4575c6708805126f
Amol Bhadane <asbhadane21@gmail.com>
2020-07-06 00:22:01 +0530

    Heroku

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import Gallery
 from .models import Post
 # Register your models here.
+admin.site.site_header = "Friend'sZone"
+
 admin.site.register(Post)
 admin.site.register(Gallery)
--- blog/forms.py
+++ blog/forms.py
@@ -9,14 +9,16 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ['title', 'text',]
+        fields = ['title', 'text','hotel_Main_Img',]
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=200, help_text="Enter Your Offitial Email")
+    first_name = forms.CharField(max_length=15, label='First name')
+    last_name = forms.CharField(max_length=15, label='Last name')
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2',]        
+        fields = ['username', 'email','first_name' ,'last_name', 'password1', 'password2',]        
 
 from django import forms 
 from .models import *
